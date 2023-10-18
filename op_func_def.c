6033b627c56a4d8823e92a4e9aa54f88059366b9
KenNyagz <kennyagz681@gmail.com>
2023-10-18 21:47:33 +0300

    An aliased Commit to save time

--- op_func_def.c
+++ op_func_def.c
@@ -73,8 +73,10 @@ void pop(stack_t **stack, unsigned int line)
 	stack_t *temp;
 
 	if (stack == NULL || *stack == NULL)
+	{
 		fprintf(stderr, "L%d: can't pop an empty stack\n", line);
-
+		exit(EXIT_FAILURE);
+	}
 	temp = *stack;
 	*stack = temp->next;
 	if (*stack != NULL)
@@ -92,6 +94,9 @@ void pop(stack_t **stack, unsigned int line)
 void show_top(stack_t **stack, unsigned int line)
 {
 	if (stack == NULL || *stack == NULL)
+	{
 		fprintf(stderr, "L%d: can't pint, stack empty\n", line);
+		exit(EXIT_FAILURE);
+	}
 	printf("%d\n", (*stack)->n);
 }
