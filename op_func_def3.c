90f370e04d3c98d33f357f2ebb1938c4914e6c04
KenNyagz <kennyagz681@gmail.com>
2023-10-19 23:21:51 +0300

    pchar opcode

--- op_func_def3.c
+++ op_func_def3.c
@@ -15,9 +15,9 @@ void pchar(stack_t **stack, unsigned int line)
 	}
 	if ((*stack)->n < 0 || (*stack)->n > 127)
 	{
-		fprintf(stderr, "L%u: can't pchar, stack empty\n", line);
+		fprintf(stderr, "L%u: can't pchar, value out of range\n", line);
 		exit(EXIT_FAILURE);
 	}
-	printf("%c\n",(*stack)->n);
+	printf("%c\n", (*stack)->n);
 
 }
